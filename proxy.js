4d6e2fe821766c194e5ee7b786e709e9d6e6a487
George Kemp <gkemp@hubspot.com>
2020-11-25 13:17:37 -0700

    Updates

--- proxy.js
+++ proxy.js
@@ -43,7 +43,7 @@ app.get('/proxy/health', (_, res) => {
   setInterval(getTokens, 1.8e+6);
   config.forEach(({ target, path }) => {
     console.log(path);
-    app.use(createProxyMiddleware(`/${path}`, {
+    app.use(createProxyMiddleware(`/${path !== '*' ? path : ''}`, {
       target,
       pathRewrite: path !== '*' ? {
         [`^/${path}`]: '/',
