adee03729ac01ff7d2bb3702623fd634bb5bd08d
George Kemp <gkemp@hubspot.com>
2020-11-25 12:37:51 -0700

    Update Proxy

--- proxy.js
+++ proxy.js
@@ -2,32 +2,61 @@ const axios = require('axios');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = require('express')();
 
-const { PORT = 8080, TARGET } = process.env;
+const { PORT = 80 } = process.env;
 
-let token;
+let tokens = {};
+let config = null;
 
-const refreshToken = async () => {
-  console.log(`[Proxy] Fetching Token`);
-  const { data } = await axios.get(`http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=${TARGET}`, {
+const getTokens = async () => {
+  console.log(`[Proxy] Fetching Tokens`);
+  config.forEach(async ({ target }) => {
+    const { data } = await axios.get(`http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=${target}`, {
+      headers: {
+        'Metadata-Flavor': 'Google'
+      }
+    });
+    token[target] = data;
+  });
+};
+
+const getConfig = async () => {
+  console.log(`[Proxy] Fetching Config`);
+  const { data } = await axios.get(`http://metadata.google.internal/computeMetadata/v1/instance/proxyconfig`, {
     headers: {
       'Metadata-Flavor': 'Google'
     }
   });
-  console.log(`[Proxy] Fetched Token`);
-  token = data;
-}
 
-// Refresh Token Immediately & then every half hour
-refreshToken();
-setInterval(refreshToken, 1.8e+6)
+  config = typeof data === 'object' ? data : JSON.parse(data);
+  console.log(config);
+};
 
-app.use('*', createProxyMiddleware({
-  target: TARGET,
-  changeOrigin: true,
-  onProxyReq: async function (proxyReq) {
-    proxyReq.setHeader('Authorization', `Bearer ${token}`);
-  }
-}));
+app.send('/proxy/config', (_, res) => {
+  res.send(config)
+});
+
+app.get('/proxy/health', (_, res) => {
+  res.status(200).send();
+});
+
+(() => {
+  await getConfig();
+  await getTokens();
+  setInterval(getTokens, 1.8e+6);
+  config.forEach(({ target, path }) => {
+    app.use(path, createProxyMiddleware({
+      target: target,
+      pathRewrite: path !== '*' ? {
+        // Remove Base Path
+        [`^/${path}/`]: '/',
+      } : undefined,
+      changeOrigin: true,
+      onProxyReq: async function (proxyReq) {
+        proxyReq.setHeader('Authorization', `Bearer ${tokens[target]}`);
+      }
+    }));
+  });
+})();
 
 app.listen(PORT, () => {
   console.log(`Application Listening on ${PORT}`);
